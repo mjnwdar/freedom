8e33a0d1eeee962c29bdfe11f27b078476674ce8
Eason <Eason@DESKTOP-HBNVJ8J>
2015-10-31 23:55:35 -0700

    commit

--- example/src/main/java/cn/springmvc/service/impl/UserServiceImpl.java
+++ example/src/main/java/cn/springmvc/service/impl/UserServiceImpl.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Service;
 import cn.springmvc.dao.UserDAO;
 import cn.springmvc.model.User;
 import cn.springmvc.service.UserService;
- 
- 
+
 @Service
 public class UserServiceImpl implements UserService{
  
@@ -15,7 +14,6 @@ public class UserServiceImpl implements UserService{
     private UserDAO userDAO;
      
     public int insertUser(User user) {
-        // TODO Auto-generated method stub
         return userDAO.insertUser(user);
     }
  
